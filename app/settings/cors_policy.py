635a98eb1c1a7d70eeabb67b48f26c2448c248da
fabix <fabiola.palmas@gmail.com>
2025-02-22 15:08:10 +0000

    Updated cors policy origins with domain of prod enviornment

--- app/settings/cors_policy.py
+++ app/settings/cors_policy.py
@@ -1,6 +1,6 @@
 cors_resource = {
 
-    r"/*": {"origins": ["https://127.0.0.1", "https://www.googleapis.com/", "http://127.0.0.1"],
+    r"/*": {"origins": ["https://fabiola.dev", "https://www.googleapis.com/", "https://www.fabiola.dev"],
             "methods": ["GET", "POST", "PUT", "DELETE"],
             "allow_headers": ["Content-Type", "Authorization"],
             "supports_credentials": True
